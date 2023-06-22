77f1ba85fa282d3b615c962b32c3b81672ca8d9b
aitegin <zhanybaev1211@gmail.com>
2023-06-21 23:26:55 -0400

    navbar start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import MainRoutes from './Routes';
+import './styles/style.css';
+const background = require('./assets/background.mp4');
+
 
 function App() {
   return (
     <div className="App">
-      <MainRoutes/>
+      <video id="background-video" autoPlay loop muted>
+        <source src={background} type="video/mp4"/>
+      </video>
+      <main>
+        <MainRoutes/>
+      </main>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,48 @@
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faComments, faMartiniGlass, faMugHot, faMugSaucer, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to='/'> Home</Link>
-                </li>
-                <li>
-                    <Link to='/list' >List</Link>
-                </li>
-            </ul>
-        </div>
+        <nav>
+            <div>
+                <FontAwesomeIcon icon={faMugSaucer} />
+                Wave Cafe
+            </div>
+            <NavLink className='nav__item' to='/drink-menu'>
+                <FontAwesomeIcon icon={faMugHot} />
+                <div>   
+                    Drink Menu
+                </div>
+            </NavLink>
+            <NavLink className='nav__item' to='/about'>
+                <FontAwesomeIcon icon={faUsers} />
+                <div>
+                    About Us
+                </div>
+            </NavLink>
+            <NavLink className='nav__item' to="/special-menu">
+               <FontAwesomeIcon icon={faMartiniGlass} />
+                <div>
+                    Special Items
+                </div>
+            </NavLink>
+            <NavLink className='nav__item' to='/contact'>
+                <FontAwesomeIcon icon={faComments} />
+                <div>
+                    Contact
+                </div>
+            </NavLink>
+            <NavLink className='nav__item' to="/cart">
+                <FontAwesomeIcon icon={faCartShopping} />
+                <div>
+                    Cart
+                </div>
+            </NavLink>
+            <div>
+                Log in
+            </div>
+        </nav>
     );
 };
 
