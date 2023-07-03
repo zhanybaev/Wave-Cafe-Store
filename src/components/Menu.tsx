a730fccd440de9440cd78b9fd5d55e1ae9ae3136
aitegin <zhanybaev1211@gmail.com>
2023-07-02 23:29:43 -0400

    link to admin page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,13 +3,32 @@ import List from './List';
 import { useAppSelector } from '../store';
 import { DRINKS_API } from '../utils/consts';
 import FilterBar from './FilterBar';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+const coffeeIcon = require('../assets/images/coffee-pot.svg') 
 
 const Menu = () => {
     const products = useAppSelector(state=>state.product.products)
     const [type, setType] = useState('Iced Coffee')
-
+    const user:string = 'admin'
+        
     return (
         <section className='menu'>
+            {
+                user==='admin' ? 
+                    <div className='navAdd' >
+                    <Link to="/add">
+                        <div className="icons">
+                            <FontAwesomeIcon style={{color:'white'}} icon={faPlus} />
+                            <img width={35} src={coffeeIcon.default} alt='coffee icon' />
+                        </div>
+                        <span>Add beverage</span>
+                    </Link>
+                    </div>                    
+                : 
+                    <></>
+            }
             <FilterBar type={type} setType={setType} />
             <List products={products} API={DRINKS_API} />
         </section>
