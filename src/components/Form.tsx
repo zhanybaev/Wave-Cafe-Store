955d2197e5d38a5883b0394b9f4f8f393e58736e
aitegin <zhanybaev1211@gmail.com>
2023-07-11 23:49:57 -0400

    edit modal ui

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,15 +12,31 @@ interface IFormProps{
 const Form = ({formHandler, titleRef, typeRef, priceRef, imgRef, descrRef}:IFormProps) => {
     return (
         <form onSubmit={(e)=>formHandler(e)}>
-            <input ref={titleRef} required placeholder='Title'/>
-            <select ref={typeRef}>
-                <option value="Hot Coffee">Hot Coffee</option>
-                <option value="Iced Coffee">Iced Coffee</option>
-                <option value="Fruit Juice">Fruit Juice</option>
-            </select>
-            <input step={'any'} ref={priceRef} required type="Number" placeholder='Price'/>
-            <input ref={imgRef} required placeholder='Image'/>
-            <textarea rows={6} ref={descrRef} required placeholder='Description'></textarea>
+            <div>
+                <label htmlFor="title">Title</label>
+                <input ref={titleRef} required id='title'/>
+            </div>
+            <div>
+                <label htmlFor="type">Type</label>
+                <select defaultValue={'DEFAULT'} ref={typeRef} id="type" >
+                    <option value="DEFAULT" disabled> </option>
+                    <option value="Hot Coffee">Hot Coffee</option>
+                    <option value="Iced Coffee">Iced Coffee</option>
+                    <option value="Fruit Juice">Fruit Juice</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="price">Price</label>
+                <input min={0} step={'any'} ref={priceRef} required type="Number" id='price'/>
+            </div>
+            <div>
+                <label htmlFor="img">Image</label>
+                <input ref={imgRef} required id='img'/>
+            </div>
+            <div>
+                <label htmlFor="description">Description</label>
+                <textarea rows={6} ref={descrRef} required id='description'></textarea>
+            </div>
             <button type='submit'>Add</button>
         </form>
     );
