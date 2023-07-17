82c7da31e3ba9b81a2ccd21e8b0b571b8c6b3a8d
aitegin <zhanybaev1211@gmail.com>
2023-07-16 23:25:34 -0400

    auth page markup done

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Auth = () => {
+	const [isLogin, setIslogin] = useState(true)
+	const handleCheckBox =()=>setIslogin(prev=>!prev)
+
     return (
         <div className="section authForm">
 		<div className="container">
@@ -7,13 +12,16 @@ const Auth = () => {
 					<div className="section pb-5 pt-5 pt-sm-2 text-center">
 						<div className="controlBar">
 							<h6><span>Log In </span><span>Sign Up</span></h6>
-								{/* <div> */}
-									<input className="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+								<div>
+									<input onChange={handleCheckBox} checked={isLogin} className="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
 									<label htmlFor="reg-log"></label>
-								{/* </div> */}
+								</div>
 						</div>
 						<div className="card-3d-wrap mx-auto">
-							<div className="card-3d-wrapper">
+							<div 
+								style={{transform: isLogin ? 'rotateY(180deg)' :  'rotateY(0deg)'}}
+								className="card-3d-wrapper"
+								>
 								<div className="card-front">
 									<div className="center-wrap">
 										<div className="section text-center">
