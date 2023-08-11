575a6052b2f23449f456165b772d0aeb4acd9f21
aitegin <zhanybaev1211@gmail.com>
2023-08-11 00:32:35 -0400

    sending email function done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,31 @@
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
+import SnackBar from './SnackBar';
+
 const Contact = () => {
+    const [showBar, setShowBar] = useState(false)
+    const [text, setText] = useState(['success', 'successfully sent'])
+    const form = useRef<HTMLFormElement>(null);
+    
+    const handleSubmit = async (e:React.FormEvent) => {
+        e.preventDefault();
+
+        if(!form.current)return;
+        const serviceID = process.env.REACT_APP_GMAIL_SERVICE_ID || ''
+        const templateID = process.env.REACT_APP_GMAIL_TEMPLATE_ID || ''
+        const publicKey = process.env.REACT_APP_GMAIL_PUBLIC_KEY || ''
+        
+        try {
+            await emailjs.sendForm(serviceID, templateID, form.current, publicKey)  
+            setText(['success', 'successfully sent'])
+        } catch (error) {
+            setText(['error', `${error}`])
+        }finally{
+            setShowBar(true)
+            form.current.reset()
+        }       
+    }
+
     return (
         <section className='contact'>
             <div className='contact__text'>
@@ -8,11 +35,14 @@ const Contact = () => {
                 </p>
             </div>
             <div className='contact__form'>
-                <input placeholder='Name' required/>
-                <input type="email" placeholder='Email' required/>
-                <textarea rows={6} placeholder='Message' required></textarea>
-                <button>Submit</button>
+                <form onSubmit={handleSubmit} ref={form}>
+                    <input name="user_name" placeholder='Name' required/>
+                    <input name="user_email" type="email" placeholder='Email' required/>
+                    <textarea name="message" rows={6} placeholder='Message' required></textarea>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
+            <SnackBar showBar={showBar} text={text[1]} type={text[0]} />
         </section>
     );
 };
